ac0381e58927a504457109aaa0d23e7f24b8e37e
NovaTech4618 <abiodunabdullahi900@gmail.com>
2026-09-06 01:34:03 -0700

    style: refine application workspace layout

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -10,9 +10,13 @@ export default function AppLayout({
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
+      <SidebarInset className="min-h-screen bg-[var(--background)]">
         <Header />
-        <main className="flex-1 overflow-y-auto p-8">{children}</main>
+        <main className="flex-1 overflow-y-auto">
+          <div className="mx-auto w-full max-w-[1600px] px-4 py-6 sm:px-6 lg:px-8 lg:py-8">
+            {children}
+          </div>
+        </main>
       </SidebarInset>
     </SidebarProvider>
   );
